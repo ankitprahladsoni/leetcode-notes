e4975f8c66cab276498ceec89c4eba7e9123fae4
Ankit Soni <ankitprahladsoni@gmail.com>
2021-04-12 01:05:51 -0700

    update config files

--- hello.py
+++ hello.py
@@ -1,3 +1,41 @@
-msg = "Hello World"
-print(msg)
-print(msg)
+from typing import List
+import heapq
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    result = 0
+
+    def dfs(self, root, maxVal):
+
+        if not root:
+            return
+
+        if root.val >= maxVal:
+            self.result += 1
+        maxVal = max(root.val, maxVal)
+        dfs(root.left, maxVal)
+        dfs(root.right, maxVal)
+
+    def goodNodes(self, root: TreeNode) -> int:
+
+        dfs(root, root.val)
+        return self.result
+
+
+obj = Solution()
+
+c = 1
+a = []
+b = [1]
+
+if c:
+    print("a")
+else:
+    print(" no a ")
